3227a183b3234c6279a387969c177bd42f3a13fd
Carlos Villadiego <carlovinar93@gmail.com>
2023-03-14 21:57:55 -0500

    se creo el fondo del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent, title:'Login - Trello'},
+  {path:'**', pathMatch:'full', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
